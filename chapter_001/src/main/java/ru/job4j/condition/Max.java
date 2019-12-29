80380ce802e26e3ca4c073c87ea90e3869b313cb
Aleksei Bulatov <bulatov2803@gmail.com>
2019-12-29 18:09:50 +0300

    Максимум из двух чисел[#211145]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -2,7 +2,6 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-            boolean greatEq = left >= right;
-        return greatEq ? left : right;
+                  return left >= right ? left : right;
     }
    }
