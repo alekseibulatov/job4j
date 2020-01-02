2fd07e032cba8e2d4de842e277286b50b353ba5e
Aleksei Bulatov <bulatov2803@gmail.com>
2020-01-03 00:39:57 +0300

    Создать программу, вычисляющую факториал.[#211213]

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public int calc(int n){
+        int result = 1 ;
+        for (n = n;n > 0; n = n - 1){
+            result = result * n;
+        }
+        return result;
+    }
+}
