2320d20dd2031af87654602913818020e0f5ae83
Aleksei Bulatov <bulatov2803@gmail.com>
2020-01-04 20:03:33 +0300

    Простое число.[#211210]

---
+++ chapter_001/src/test/java/ru/job4j/loop/CheckPrimerNumberTest.java
@@ -0,0 +1,22 @@
+package ru.job4j.loop;
+import  org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class CheckPrimerNumberTest {
+    @Test
+    public void when5(){
+        boolean rs1 = CheckPrimerNumber.check(5);
+        assertThat(rs1, is(true));
+    }
+    @Test
+    public void when4(){
+        boolean rs1 = CheckPrimerNumber.check(4);
+        assertThat(rs1, is(false));
+    }
+    @Test
+    public void when2(){
+        boolean rs1 = CheckPrimerNumber.check(2);
+        assertThat(rs1, is(true));
+    }
+}
