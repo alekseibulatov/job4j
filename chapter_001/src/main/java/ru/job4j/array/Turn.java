d8fdb8236d3f31856686e7c4cf2d907b49861204
Aleksei Bulatov <bulatov2803@gmail.com>
2020-01-05 23:22:45 +0300

    Перевернуть массив.[#211165]

---
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -0,0 +1,13 @@
+package ru.job4j.array;
+
+public class Turn {
+    public int[] back(int[] array){
+        for (int index = 0; index < array.length - index - 1; index++){
+
+          int temp =   array[array.length -index - 1];
+         array[array.length -index - 1] = array[index];
+         array[index ] = temp;
+        }
+        return array;
+    }
+}
