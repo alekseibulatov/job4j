d963a8dcca38d0f9a8d90c5deba4699e386bf4fe
Aleksei Bulatov <bulatov2803@gmail.com>
2020-01-06 17:33:42 +0300

    Слово заканчивается с ...[#211157]

---
+++ chapter_001/src/test/java/ru/job4j/array/EndsWithTest.java
@@ -0,0 +1,21 @@
+package ru.job4j.array;
+import  org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class EndsWithTest {
+    @Test
+    public void whenStartWithPrefixThenTrue(){
+        char[] word = {'H', 'e' ,'l' ,'l', 'o'};
+        char[] post = {'l' ,'o'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(true));
+    }
+    @Test
+    public void whenStartWithPrefixThenFalse(){
+        char[] word = {'H', 'e' ,'l' ,'l', 'o'};
+        char[] post = {'l' ,'a'};
+        boolean result = EndsWith.endsWith(word, post);
+        assertThat(result, is(false));
+    }
+}
