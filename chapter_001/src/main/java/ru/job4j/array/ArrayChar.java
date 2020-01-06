205095efe563554cfabb3ed629542a3dc7d7e441
Aleksei Bulatov <bulatov2803@gmail.com>
2020-01-07 01:08:01 +0300

    Слово начинается с ...[#211153]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,12 +4,11 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref){
         boolean result = false;
         for (int i = 0; i < pref.length; i++){
-            if (word[i] == pref[i]){
-                result = true;
-            } else {
+            if (word[i] != pref[i]){
                 result = false;
                 break;
-            }
+            } else
+                result = true;
         }
         return result;
     }
