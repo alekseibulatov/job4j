c443c7320dbff7d969e683969897aaf1dcdf1952
Aleksei Bulatov <bulatov2803@gmail.com>
2020-02-09 00:44:51 +0300

    works at a home

--- chapter_002/src/main/java/ru/job4j/oop/Dog.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.oop;
-
-public class Dog {
-    public static void main(String[] args) {
-        Dog polkan = new Dog();
-        Dog sharic = new Dog();
-        Dog zhychka = new Dog();
-    }
-}
