080d48871bb01c240ea68e81e8ad5cfa7dcbda37
Alexei Bulatov <bulatov2803@gmail.com>
2020-04-23 17:29:59 +0300

    Удаление моделей из массива.[#211311]

--- chapter_002/src/main/java/ru/job4j/pojo/Shop.java
+++ chapter_002/src/main/java/ru/job4j/pojo/Shop.java
@@ -1,5 +1,6 @@
 package ru.job4j.pojo;
 
+
 public class Shop {
     public static void main(String[] args) {
         Product products[] = new Product[5];
@@ -47,34 +48,34 @@ public class Shop {
 
         System.out.println("Метод delete" + System.lineSeparator());
 
+        System.out.println("Массив продуктов до удаления ячейки");
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
+
         delete(products, 1);
 
+        System.out.println("Массив продуктов после удаления ячейки с индексом index");
+        for (int i = 0; i < products.length; i++) {
+            Product product = products[i];
+            if (product != null) {
+                System.out.println(product.getName());
+            } else {
+                System.out.println("null");
+            }
+        }
     }
 
     public static Product[] delete(Product[] products, int index) {
-
-        products[index] = null;
-        for (int i = 0; i < products.length - 1; i++) {
-
-            if (i < index) { //
-                Product product = products[i];
-                if (product != null) {
-                    System.out.println(product.getName());
-                } else {
-                    System.out.println("null");
-                }
-            } else if (i >= index) {
-                products[i] = products[i + 1];
-                Product product = products[i];
-                if (product != null) {
-                    System.out.println(product.getName());
-                } else {
-                    System.out.println("null");
-                }
-            }
+        for (int i = index; i < products.length - 1; i++) {
+            products[i] = products[i + 1];
         }
         products[products.length - 1] = null;
-        System.out.println(products[products.length - 1]);
         return products;
     }
 }
