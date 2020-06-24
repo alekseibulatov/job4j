c128fc3124262e3918213d8cd3f077166562fed6
Aleksei Bulatov <bulatov2803@gmail.com>
2020-06-24 23:29:08 +0300

    Рефакторинг теста @Before @After [#211263]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,6 +2,11 @@ package ru.job4j.tracker;
 
 
 public class StartUI {
+    private final Output out;
+
+    public StartUI(Output out) {
+        this.out = out;
+    }
 
     public void init(Input input, Tracker tracker, UserAction[] actions) {
         boolean run = true;
@@ -17,18 +22,19 @@ public class StartUI {
         System.out.println("Menu.");
         for (int index = 0; index < actions.length;
              index++) {
-            System.out.println(index + "." + actions[index].name());
+            out.println(index + "." + actions[index].name());
         }
     }
 
     public static void main(String[] args) {
+        Output output = new ConsoleOutput();
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
-        UserAction[] actions = {new CreateAction(), new ShowAllAction(),
+        UserAction[] actions = {new CreateAction(output), new ShowAllAction(),
                 new EditAction(), new DeleteAction(), new FindActionById(),
                 new FindActionByName(), new ExitAction()};
 
-        new StartUI().init(input, tracker, actions);
+        new StartUI(output).init(input, tracker, actions);
     }
 }
 
