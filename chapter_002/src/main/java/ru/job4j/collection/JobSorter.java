94801f5ae78ab6bdde6e3724b00c9bc0fa323f16
alekseibulatov <59173362+alekseibulatov@users.noreply.github.com>
2020-11-02 00:10:57 +0300

    Сортировка Job с использованием Comparator [#211231]

--- chapter_002/src/main/java/ru/job4j/collection/JobSorter.java
+++ chapter_002/src/main/java/ru/job4j/collection/JobSorter.java
@@ -11,10 +11,22 @@ public class JobSorter {
                 new Job("Impl task", 2),
                 new Job("Reboot server", 1));
         System.out.println(jobs);
-        Collections.sort(jobs);
+        Collections.sort(jobs, new SortByUpNameJob());
         System.out.println(jobs);
-        Collections.sort(jobs, new SortByNameJob());
+        Collections.sort(jobs, new SortByDownNameJob());
         System.out.println(jobs);
-     }
+        Collections.sort(jobs, new SortByUpPriorityJob());
+        System.out.println(jobs);
+        Collections.sort(jobs, new SortByDownPriorityJob());
+        System.out.println(jobs);
+        List<Job> jobs1 = Arrays.asList(
+                new Job("Fix bugs", 4),
+                new Job("Fix bugs", 2),
+                new Job("Reboot server", 1));
+        Collections.sort(jobs1, new SortByUpNameJob().thenComparing(new SortByUpPriorityJob()));
+        System.out.println(jobs1);
+        Collections.sort(jobs1, new SortByUpNameJob().thenComparing(new SortByDownPriorityJob()));
+        System.out.println(jobs1);
+    }
 
 }
--- chapter_002/src/main/java/ru/job4j/collection/SortByDownNameJob.java
+++ chapter_002/src/main/java/ru/job4j/collection/SortByDownNameJob.java
@@ -2,11 +2,10 @@ package ru.job4j.collection;
 
 import java.util.Comparator;
 
-public class SortByNameJob implements Comparator<Job> {
-
+public class SortByDownNameJob implements Comparator<Job> {
 
     @Override
     public int compare(Job first, Job second) {
-        return first.getName().compareTo(second.getName());
+        return second.getName().compareTo(first.getName());
     }
 }
