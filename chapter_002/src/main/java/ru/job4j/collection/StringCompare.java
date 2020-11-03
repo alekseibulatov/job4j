bb9b24f4c81555c050c43b078d465bd906423ece
Aleksei Bulatov <bulatov2803@gmail.com>
2020-11-03 17:09:05 +0300

    Компаратор для строк. [#211229]

--- chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
+++ chapter_002/src/main/java/ru/job4j/collection/StringCompare.java
@@ -1,52 +1,27 @@
 package ru.job4j.collection;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
-import java.util.List;
+
 
 public class StringCompare implements Comparator<String> {
 
 
     @Override
     public int compare(String left, String right) {
-        char[] ll = left.toCharArray();
-        int value = -1;
-
-        List<Character> l = new ArrayList<Character>();
-        for (char x : ll) {
-            l.add(x);
-        }
-        char[] rr = right.toCharArray();
-
-        List<Character> r = new ArrayList<Character>();
-        for (char z : rr) {
-            r.add(z);
-        }
-        if (l.size() <= r.size()) {
-            while (l.size() < r.size()) {
-                l.add('0');
-            }
-            for (int i = 0; i < r.size(); i++) {
-                value = Character.compare(l.get(i), r.get(i));
-                if (value == 0) {
-                    continue;
-                } else
-                    return value;
+        int l = Math.min(left.length(), right.length());
+        int rsl = -1;
+        for (int i = 0; i < l; i++) {
+            char q = left.charAt(i);
+            char w = right.charAt(i);
+            rsl = Character.compare(q, w);
+            if (rsl != 0) {
+                break;
+            } else if (rsl == 0 || i == l - 1) {
+                rsl = Integer.compare(left.length(), right.length());
+            } else if (rsl == 0) {
+                continue;
             }
         }
-        if (r.size() < l.size()) {
-            while (r.size() < l.size()) {
-                r.add('0');
-            }
-            for (int i = 0; i < r.size(); i++) {
-                value = Character.compare(l.get(i), r.get(i));
-                if (value == 0) {
-                    continue;
-                } else
-                    return value;
-            }
-        }
-        return value;
+        return rsl;
     }
 }
