d6e4ae0b8714b06db4347c88c94a44b3bfd53de6
Aleksei Bulatov <bulatov2803@gmail.com>
2020-11-05 19:18:08 +0300

    Банковские переводы. [#211235]

---
+++ chapter_002/src/main/java/ru/job4j/bank/BankService.java
@@ -0,0 +1,35 @@
+package ru.job4j.bank;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BankService {
+    private Map<User, List<Account>> users = new HashMap<>();
+
+    public void addUser(User user) {
+        if (){
+
+        }
+        users.put(user, new ArrayList<Account>());
+    }
+
+    public void addAccount(String passport, Account account) {
+
+    }
+
+    public User findByPassport(String passport) {
+        return null;
+    }
+
+    public Account findByRequisite(String passport, String requisite) {
+        return null;
+    }
+
+    public boolean transferMoney(String srcPassport, String srcRequisite,
+                                 String destPassport, String destRequisite, double amount) {
+        boolean rsl = false;
+        return rsl;
+    }
+}
