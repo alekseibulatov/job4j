cac51da3371ad97d5315c9b16b7e66682b294ddf
Aleksei Bulatov <bulatov2803@gmail.com>
2020-11-18 14:16:29 +0300

    Контрольные вопросы часть 3. Collections. Lite.

--- chapter_002/src/main/java/ru/job4j/collection/Article.java
+++ chapter_002/src/main/java/ru/job4j/collection/Article.java
@@ -1,32 +1,20 @@
 package ru.job4j.collection;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Article {
     public static boolean generateBy(String origin, String line) {
-        Map<Integer, String> hashmapOrigin = new HashMap<Integer, String>();
-        Map<Integer, String>  hashmapLine = new HashMap<Integer, String>();
-        String[] originArray = origin.split(" |\\.|\\,|\\!");
+        Set<String> hashmapOrigin = new HashSet<>();
+        Set<String> hashmapLine = new HashSet<>();
+        origin = origin.replaceAll("\\p{P}", " ");
+        String[] originArray = origin.split(" ");
         String[] l = line.split(" ");
-        int i = 0;
-        for (String s : originArray) {
-            hashmapOrigin.put(i, s);
-            i++;
-        }
-        for (String d : l) {
-            hashmapLine.put(i, d);
-            i++;
-        }
-        boolean rsl = false;
-        for (String z : l) {
-            if (hashmapOrigin.containsValue(z)) {
-                rsl = true;
-            } else {
-                rsl = false;
-                break;
-            }
-        }
+        hashmapOrigin.addAll(Arrays.asList(originArray));
+        hashmapLine.addAll(Arrays.asList(l));
+        boolean rsl;
+        rsl = hashmapOrigin.containsAll(hashmapLine);
         return rsl;
     }
 }
