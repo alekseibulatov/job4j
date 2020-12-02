c356c4df031ba9b93e328d4274a2d2ef2f908bd0
Aleksei Bulatov <bulatov2803@gmail.com>
2020-12-02 15:47:55 +0300

    Крестики-нолики на JavaFX [#211128]

---
+++ chapter_002/src/main/java/ru/job4j/stream/Analyze.java
@@ -0,0 +1,64 @@
+package ru.job4j.stream;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Analyze {
+    /**
+     * Метод вычисляет общий средний балл.
+     *
+     * @param stream
+     * @return
+     */
+
+    public static double averageScore(Stream<Pupil> stream) {
+        return stream.flatMap(x -> x.getSubjects().stream()).mapToInt(Subject::getScore).average().orElse(0D);
+    }
+
+    /**
+     * Метод вычисляет средний балл ученика по его предметам
+     *
+     * @param stream
+     * @return - список из объектов Tuple (имя ученика и средний балл)
+     */
+
+    public static List<Tuple> averageScoreBySubject(Stream<Pupil> stream) {
+      return   stream.map(x -> new Tuple(x.getName(), x.getSubjects().stream().mapToDouble(Subject::getScore).
+
+    }
+
+    /**
+     * Метод вычисляет средний балл по всем предметам для каждого ученика
+     *
+     * @param stream
+     * @return - список из объектов Tuple (название предмета и средний балл)
+     */
+
+    public static List<Tuple> averageScoreByPupil(Stream<Pupil> stream) {
+        return List.of();
+    }
+
+    /**
+     * Метод возвращает лучшего ученика.
+     * Лучшим считается ученик с наибольшим баллом по всем предметам.
+     *
+     * @param stream
+     * @return
+     */
+
+    public static Tuple bestStudent(Stream<Pupil> stream) {
+        return null;
+    }
+
+    /**
+     * Метод возвращает предмет с наибольшим баллом для всех студентов.
+     *
+     * @param stream
+     * @return -обект  Tuple (имя предмета, сумма баллов каждого ученика по этому предмету)
+     */
+
+    public static Tuple bestSubject(Stream<Pupil> stream) {
+        return null;
+    }
+}
